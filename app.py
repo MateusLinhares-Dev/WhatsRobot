1e984031194f98930d99862aaed7e7b725b8b3bf
Mateus Epifanio Linhares <83597337+MateusLinhares-Dev@users.noreply.github.com>
2024-07-03 09:20:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def whatsapp_webhook():
 def create_workflow(workflow_title):
     # Criar workflow
     workflow_formatted = workflow_title
-    __url_sesuite = 'https://isc.softexpert.com/apigateway/se/exp/chatbot/api/instance.php?'
-    __header = {'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDk3NTMyNDIsImV4cCI6MTc2NzIyNTU0MCwiaWRsb2dpbiI6Im1hdGV1cy5saW5oYXJlcyIsInJhdGVsaW1pdCI6MTIwLCJxdW90YWxpbWl0IjoxMDAwMDB9.JlBd2kreOy8ArX1M-QLobAQaIdPBmZvUSEndKa61Qxo'}
+    __url_sesuite = '?'
+    __header = {'Authorization': ''}
     __data = f'idProcess=AE001&cdProduct=39&nmInstance={workflow_formatted}'
     __url_data = f'{__url_sesuite}{__data}'
     __response = requests.post(__url_data, headers=__header)
